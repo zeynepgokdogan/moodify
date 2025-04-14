097d568ce61bc10b7eac8cb07b17318d9c27e062
Zeynep <88207798+zeynepgokdogan@users.noreply.github.com>
2025-04-14 11:39:37 +0300

    Initial Commit

---
+++ moodify/moodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodifyApp.swift
+//  moodify
+//
+//  Created by Zeynep Gökdoğan on 14.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct moodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moodifyTests/moodifyTests.swift
@@ -0,0 +1,16 @@
+//
+//  moodifyTests.swift
+//  moodifyTests
+//
+//  Created by Zeynep Gökdoğan on 14.04.2025.
+//
+
+import Testing
+
+struct moodifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
